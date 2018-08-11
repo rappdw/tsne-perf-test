1e3ba1f4f728006b36381dd806fbd08c256e113e
Daniel Rapp <rappdw@gmail.com>
2018-08-11 13:22:27 -0600

    Dwr/include pypi ver (#2)
    * Add danielfrg (version on pypi) to comparision
    
    * Update timings on m5.12xlarge
    
    * Update timings on m5.12xlarge

--- README.md
+++ README.md
@@ -7,26 +7,29 @@ between different implementations of BH-tSNE.
 The versions that are compared are:
 * [lvdmaaten](https://github.com/lvdmaaten/bhtsne.git) (e53ec46d9...)
 * [10XDev](https://github.com/10XDev/tsne.git) (1858079da...)
+* [danielfrg](https://github.com/danielfrg/tsn.git) (v0.1.8) (This is the library on pypi)
 * [rappdw](https://github.com/rappdw/tsne.git) (d7447950...)
 
 To run the test:
 1) Build to docker image: `docker build -t tsne-perf .`
 2) Run the image, specifying which data set to use, e.g. `docker run -it tsne-perf iris`
 
-# On an EC2 M4.4xlarge instance
+# On an EC2 m5.12xlarge instance
 ## 2500 Instance MNIST Data 
 
-| Repo | Wall Time (s) | Max Memory (kb) | Cumulative CPU % |
-| ---- | ------------- | ----------- | ---------------- |
-| lvdmaaten | 18.54 | 14676 | 100 |
-| 10XDev | 10.19 | 14916 | 100 |
-| rappdw | 8.05 | 16964 | 4560 |
+| Repo      | Wall Time (s) | Max Memory (kb) | Cumulative CPU % |
+| --------- | ------------- | --------------- | ---------------- |
+| lvdmaaten | 16.04         | 14516           | 99               |
+| danielfrg | 7.80          | 34096           | 99               |
+| 10XDev    | 7.52          | 14612           | 99               |
+| rappdw    | 7.62          | 11580           | 4347             |
 
 ## 70,000 Instance MNIST Data 
 
-| Repo | Wall Time (s) | Max Memory (kb) | Cumulative CPU % |
-| ---- | ------------- | ----------- | ---------------- |
-| lvdmaaten | 5646.54 | 1426872 | 99 |
-| 10XDev | 3758.63 | 1426960 | 99 |
-| rappdw | 490.05 | 1449816 | 4360 |
+| Repo      | Wall Time (s) | Max Memory (kb) | Cumulative CPU % |
+| --------- | ------------- | --------------- | ---------------- |
+| lvdmaaten | 6064.91       | 1426784         | 99               |
+| 10XDev    | 3753.59       | 1426692         | 99               |
+| danielfrg | 2100.58       | 1426288         | 99               |
+| rappdw    | 329.98        | 1436172         | 3588             |
 
--- tsne-perf-test.py
+++ tsne-perf-test.py
@@ -8,6 +8,7 @@ import shlex
 def run_test(tsne_dir, outfile):
     if not subprocess.call(shlex.split('cd {}; make clean all'.format(tsne_dir)), shell=True):
         tsne_command = '/usr/bin/time -f "%e %M %P" -o {} {}/bh_tsne'.format(outfile, tsne_dir)
+        print("running command: {}".format(tsne_command))
         subprocess.call(shlex.split(tsne_command), shell=False)
 
 
@@ -15,30 +16,30 @@ def print_file(file, test_name):
     with open(file, 'r') as fin:
         print('{}: {}'.format(test_name, fin.read()))
 
+if __name__ == '__main__':
 
-parser = argparse.ArgumentParser()
-parser.add_argument("dataset", choices=['full.mnist', '2500.mnist', 'iris'], help="dataset to use",
-                    default='full.mnist')
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", choices=['full.mnist', '2500.mnist', 'iris'], help="dataset to use",
+                        default='full.mnist')
+    args = parser.parse_args()
 
-data_file = "/sandbox/data.{}.dat".format(args.dataset)
+    data_file = "/sandbox/data.{}.dat".format(args.dataset)
 
-copyfile(data_file, 'data.dat')
+    copyfile(data_file, 'data.dat')
 
-out_rappdw = '/sandbox/time.rappdw.out'
-out_rappdw_noopenmp = '/sandbox/time.rappdw.noopenmp.out'
-out_10x = '/sandbox/time.10XDev.out'
-out_lvdmaaten = '/sandbox/time.lvdmaaten.out'
+    comparisons = [
+        ('rappdw', '/sandbox/tsne.rappdw/', '/sandbox/time.rappdw.out'),
+        ('rappdw.noopenmp', '/sandbox/tsne.rappdw.noopenmp/', '/sandbox/time.rappdw.noopenmp.out'),
+        ('danielfrg', '/sandbox/tsne.danielfrg/', '/sandbox/time.danielfrg.out'),
+        ('10XDev', '/sandbox/tsne.10XDev/', '/sandbox/time.10XDev.out'),
+        ('lvdmaaten', '/sandbox/tsne.lvdmaaten', '/sandbox/time.lvdmaaten.out'),
+    ]
 
-run_test('/sandbox/tsne.rappdw/', out_rappdw)
-run_test('/sandbox/tsne.rappdw.noopenmp/', out_rappdw_noopenmp)
-run_test('/sandbox/tsne.10XDev/', out_10x)
-run_test('/sandbox/tsne.lvdmaaten', out_lvdmaaten)
+    for comparison in comparisons:
+        run_test(comparison[1], comparison[2])
 
-print('==========================================================')
-print('\n\n')
-print_file(out_rappdw, 'rappdw')
-print_file(out_rappdw_noopenmp, 'rappdw.noopenmp')
-print_file(out_10x, '10XDev')
-print_file(out_lvdmaaten, 'lvdmaaten')
+    print('==========================================================')
+    print('\n\n')
+    for comparison in comparisons:
+        print_file(comparison[2], comparison[0])
 
